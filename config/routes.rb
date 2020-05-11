e38dc102b8ce1ccce4cf32e06dc0b45f75de9a6b
hiro0911 <evergreen.0911@gmail.com>
2020-05-12 07:09:51 +0900

    [Add] 本の投稿フォーム作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-resources :users, only: [:new, :create, :index, :show]
-resources :books, only: [:new, :create, :index, :show]
-resources :home, only: [:new, :create, :index, :show]
+resources :users
+resources :books
+resources :home
 get 'top' => 'users#top'
   root 'users#top'
   get 'home/about' => 'home#about'
