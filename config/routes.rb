135301d56a46934daac7037b7ef573c21a2473e8
hiro0911 <evergreen.0911@gmail.com>
2020-05-12 04:57:13 +0900

    [Add] topページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+resources :users, only: [:new, :create, :index, :show]
+resources :books, only: [:new, :create, :index, :show]
+resources :home, only: [:new, :create, :index, :show]
+get 'top' => 'users#top'
+  root 'users#top'
+  get 'home/about' => 'home#about'
 end
